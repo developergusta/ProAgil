48abebd62a9f404021589affb5d245db8ef6c9bf
oliveguga <olive.guga@gmail.com>
2020-06-04 21:12:56 -0300

    Inicial do meu projeto de eventos

---
+++ ProAgil.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento[]{
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "angular 1",
+                    Local = "belo horizonte",
+                    Lote = "primeiro",
+                    QtdPessoas = 200,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "node js",
+                    Local = "sampa",
+                    Lote = "segundo ",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                }
+            };
+        }
+    }
+}
